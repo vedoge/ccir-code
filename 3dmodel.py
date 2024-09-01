ecddb1f7d5b492d553889bf7b0f261087869cbf2
Vedant Ganesh <>
2024-09-01 12:10:50 +0800

    still have a few problems with centrifugal force

--- 3dmodel.py
+++ 3dmodel.py
@@ -14,35 +14,45 @@ def cart2surf(a):
 		phi = 0
 	lam = np.acos(np.sqrt(np.linalg.norm(a)/rm))
 	return np.array([lam,phi])
-
+# universal gravitational constant
 G = 6.67e-8 # dyn cm^2 g^-2
-# following parameters are of significance in the calculation
-T = 1 # s
-wmag = 2*np.pi/T # radHz
+# parameters of significance in the calculation
+f = 1 # Hz
+wmag = 2*np.pi*f # radHz
 alpha = 0 # rad
 w = wmag*np.array([np.sin(alpha),0,np.cos(alpha)]) # radHz (Cartesian)
-rm = 1e8 # cm
-rpuls = 1e6 #cm
+rm = np.array(1e8) # cm
+rpuls = np.array(1e6) #cm
 M = 2.784e33 # g
-# the following represents the initial conditions of the particle in question
+# initial conditions of the particle in question
 phi = 0
-v0 = 1e5
+v = 1e5
 dx = 0 # arclength (will be converted to delta-lambda)
 # fixed timestep 
-dt = 1e-5
+t = 0
+dt = 1e-4
 # calculate initial elevation
-lam = np.pi/2 - np.atan(1/(np.sin(phi)*np.tan(alpha)))
-chi = np.tan(0.5/np.atan(lam))
+if phi != 0 and alpha != 0:
+	lam = np.pi/2 - np.atan(1/(np.sin(phi)*np.tan(alpha)))
+	chi = np.tan(0.5/np.atan(lam))
+else: 
+	lam = 0
+	chi = np.pi/2
+r = rm*np.cos(lam)**2
 # integrate the equation of motion
-while rm*np.cos(lam)**2 >= rpuls:
-	ag = G*m_pulsar * np.cos(chi) / (r**2) 
+while np.any(r >= rpuls):
 	# calculate centrifugal using -n dot omega cross (omega cross position)
-	coord = surf2cart(lam,phi,rm) # r
-	# NEED NORM FIXME
-	norm = # n
+	ntheta = 3*np.pi/2 + lam - chi
+	nphi = phi + np.pi
+	n = np.array([np.cos(ntheta)*np.cos(nphi),np.cos(ntheta)*np.sin(nphi),np.sin(ntheta)])
+	pos = surf2cart(lam,phi,rm)
+	acen = np.dot(n,np.cross(w,np.cross(w,pos)))*n
+	print(t,v)
+	ag = n*G*M* np.cos(chi) / (r**2)
+	a = ag + acen
 	dx = v*dt + 0.5*a*(dt**2) # verlet
 	v = v + a*dt # update velocity
-	lam += dx / (rm*np.cos(lam) * (1+3*(np.sin(lam)**2))**0.5) # calculate delta-lambda based on eq. given
+	lam += np.linalg.norm(dx) / (rm*np.cos(lam) * (1+3*(np.sin(lam)**2))**0.5) # calculate delta-lambda based on eq. given
 	chi = np.atan(1/(2*np.tan(lam)))
 	r = rm * (np.cos(lam)**2)
 	t += dt

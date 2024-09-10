7c4d2242b6d1952d16193008d1799a0f9ae89ca3
vedoge <81337539+vedoge@users.noreply.github.com>
2024-09-10 21:48:26 +0800

    Found problem; commented

--- scalarfield.py
+++ scalarfield.py
@@ -5,7 +5,7 @@ G = 6.67e-8 # dyn cm^2 g^-2
 # parameters of significance in the calculation
 f = 10 # Hz
 wmag = 2*np.pi*f # radHz
-alpha = 1 # rad
+alpha = 0 # rad
 w = wmag*np.array([np.sin(alpha),0,np.cos(alpha)]) # radHz (Cartesian)
 rm = 1e8 # cm
 rpuls = 1e6 #cm
@@ -17,32 +17,35 @@ lam = np.linspace(-maxlam,maxlam,1001)
 phi = np.linspace(0,np.pi*2,1001)
 spacelam,spacephi = np.meshgrid(lam,phi)
 r = np.array([
-			(rm*np.cos(spacelam)**3)*np.cos(spacephi),
-			(rm*np.cos(spacelam)**3)*np.sin(spacephi),
+			(rm*np.cos(spacelam)**2)*np.cos(spacelam)*np.cos(spacephi),
+			(rm*np.cos(spacelam)**2)*np.cos(spacelam)*np.sin(spacephi),
 			(rm*np.cos(spacelam)**2)*np.sin(spacelam)
 			]) # mathematics convention (latitude)
 # centrifugal acceleration = -n@(w*(w*r))*n
 # gravitational acceleration = n*G*M*pos@n/(np.linalg.norm(pos)**3)
 slope_lat = np.atan(0.5/np.tan(spacelam))-spacelam # signed latitude for normal vector at each point
-n = np.array([np.cos(slope_lat)*np.cos(spacephi), np.cos(slope_lat)*np.sin(spacephi), np.sin(slope_lat)]) # mathematics convention (latitude)
-# now we need to evaluate acceleration at every point (thank god for SIMD)
-ag = -G*M*r@n/(np.linalg.norm(r,axis=0)**3)
-acen = np.zeros(r.shape) # create array full of 
-#plt.contourf(spacelam,spacephi,np.linalg.norm(ag,axis=0)) # is actually a very reasonable graph - the value explodes close to lambda=np.pi/2 (visible in the 10^50 cm^2 yellow bar on either end)
-#plt.colorbar()
-#plt.show()
-# work out how to do a cross product of r[:,i,j] with w
-# find a function lambda(phi,w,alpha,beta)
+n = np.array([
+			np.cos(slope_lat)*np.cos(spacephi+np.pi),
+			np.cos(slope_lat)*np.sin(spacephi+np.pi),
+			np.sin(slope_lat)
+			])# mathematics convention (latitude) vector along magnetic field lines for every point (lambda, phi)
+# now we need to evaluate acceleration at every point
+ag = -G*M*np.cos(slope_lat+spacelam)/(np.linalg.norm(r,axis=0)**2)
+acen = np.zeros(r.shape) # create array full of zeros in the necessary shape
 # please find a way to make this agonisingly slow job faster
 #for i in range(r.shape[1]):
 #    for j in range(r.shape[2]):
 #        acen[:,i,j] = -n[:,i,j]@np.cross(w,np.cross(w,r[:,i,j]))
-acen = -n@np.cross(w,np.cross(w,r,axis=0),axis=0)
+#w*(w*r)
+print(n.shape)
+print(w.shape)
+acen = np.cross(w,np.cross(w,r,axis=0),axis=0) #problem here dotting n with each vector
+
 a = np.linalg.norm(acen, axis=0) + np.linalg.norm(ag,axis=0)
-loga = np.log10(a)
-plt.contourf(spacelam,spacephi,loga)
+plt.contourf(spacelam,spacephi,np.log10(np.linalg.norm(acen,axis=0)))
 plt.xlabel("lambda / rad")
 plt.ylabel("phi / rad")
-plt.title("Contour plot of log(a) / log(cm s^-2)")
+plt.title("Contour plot of log centrifugal acceleration/ log(cm s^-2)")
 plt.colorbar()
 plt.show()
+#lambda* = arctan(1/(sin(phi)*tan(lambda)))

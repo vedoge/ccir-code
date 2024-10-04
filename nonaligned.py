97db15c7272e81198bfbd7fa28c6e9fb1346084b
vedoge <81337539+vedoge@users.noreply.github.com>
2024-10-04 17:45:55 +0800

    Update nonaligned.py

--- nonaligned.py
+++ nonaligned.py
@@ -1,20 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-alpha = 1 #rad
-beta = 0.5 #rad
+alpha = np.pi/2 #rad
+beta =  0 #rad
 phi,wt = np.meshgrid(np.arange(0,2*np.pi,np.pi/100),np.arange(0,2*np.pi,np.pi/100))
 zimg = np.array([np.sin(alpha)*np.sin(wt),
                  -(np.sin(beta)*np.cos(wt)*np.cos(alpha)+np.sin(alpha)*np.cos(beta)),
                  -np.sin(beta)*np.cos(wt)*np.sin(alpha)+np.cos(alpha)*np.cos(beta)
                  ])
-print(zimg.shape)
 lam0 = np.atan(np.linalg.norm(zimg,axis=0)*np.sin(phi))
 plt.plot(phi[0,:],lam0[0,:],)
 plt.plot(phi[24,:],lam0[24,:])
 plt.plot(phi[49,:],lam0[49,:])
 plt.plot(phi[74,:],lam0[74,:])
-plt.legend(['wt=0','wt=pi/2','wt=pi','wt=3pi/2'])
-plt.xlabel('phi')
-plt.ylabel('lambda')
+plt.legend([r'$\omega t=0$',r'$\omega t=pi/2$',r'$\omega t=pi$',r'$\omega t=3pi/2$'])
+plt.xlabel(r'$\varphi$ / rad')
+plt.ylabel(r'$\lambda$ / rad')
 plt.show()
 

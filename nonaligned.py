5884881e4a43281ee043db39c52268ea3a189a86
vedoge <81337539+vedoge@users.noreply.github.com>
2024-10-06 18:17:16 +0800

    sync commit

--- nonaligned.py
+++ nonaligned.py
@@ -1,19 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-alpha = np.pi/2 #rad
-beta =  0 #rad
-phi,wt = np.meshgrid(np.arange(0,2*np.pi,np.pi/100),np.arange(0,2*np.pi,np.pi/100))
-zimg = np.array([np.sin(alpha)*np.sin(wt),
-                 -(np.sin(beta)*np.cos(wt)*np.cos(alpha)+np.sin(alpha)*np.cos(beta)),
-                 -np.sin(beta)*np.cos(wt)*np.sin(alpha)+np.cos(alpha)*np.cos(beta)
-                 ])
-lam0 = np.atan(np.linalg.norm(zimg,axis=0)*np.sin(phi))
-plt.plot(phi[0,:],lam0[0,:],)
-plt.plot(phi[24,:],lam0[24,:])
-plt.plot(phi[49,:],lam0[49,:])
-plt.plot(phi[74,:],lam0[74,:])
+alpha = 1 #rad
+#10 KeV
+beta =  0.5 #rad
+chi,wt = np.meshgrid(np.arange(0,2*np.pi,np.pi/100),np.arange(0,2*np.pi,np.pi/100))
+lam0 = np.acos((np.abs(np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))**2 + np.abs(np.sin(chi)*(np.sin(alpha)*np.sin(beta) - np.cos(alpha)*np.cos(beta)*np.cos(wt)) - np.cos(beta)*np.cos(chi)*np.sin(wt))**2)**(1/2))
+plt.plot(chi[0,:],lam0[0,:])
 plt.legend([r'$\omega t=0$',r'$\omega t=pi/2$',r'$\omega t=pi$',r'$\omega t=3pi/2$'])
-plt.xlabel(r'$\varphi$ / rad')
-plt.ylabel(r'$\lambda$ / rad')
+plt.xlabel(r'$\chi$ / rad')
+plt.ylabel(r'$\lambda_0$ / rad')
 plt.show()
 
--- simul.py
+++ simul.py
@@ -12,21 +12,28 @@ rm = 1e8 # cm
 rpuls = 1e6 # cm
 # make sure rpuls is reasonable because otherwise it messes up the contour map
 M = 2.784e33 # g
-# set up 2-dimensional space
-maxlam = np.acos(np.sqrt(rpuls/rm))
-lam = np.arange(-maxlam,maxlam,maxlam/1000) # -maxlam -999maxlam/1000 ... 0 ... maxlam
-phi = np.arange(0,np.pi*2,np.pi/500) # 0 pi/1000 2pi/1000 ... 2pi
-spacelam,spacephi = np.meshgrid(lam,phi)
+alpha = 1 #rad
+beta = 0.5 #rad
+phi,wt = np.meshgrid(np.arange(0,2*np.pi,np.pi/1000),np.arange(0,2*np.pi,np.pi/1000))
+
+zimg = np.array([np.sin(alpha)*np.sin(wt),
+                 -(np.sin(beta)*np.cos(wt)*np.cos(alpha)+np.sin(alpha)*np.cos(beta)),
+                 -np.sin(beta)*np.cos(wt)*np.sin(alpha)+np.cos(alpha)*np.cos(beta)
+                 ])
+
+lam0 = np.atan(np.linalg.norm(zimg,axis=0)*np.sin(phi)) # works
+
+p# set up 2-dimensional space
+maxlam = np.acos(np.sqrt(rpuls/rm)) # works 
+np.arange(-maxlam,maxlam,maxlam/1000) # -maxlam -999maxlam/1000 ... 0 ... maxlam
+spacelam,spacephi = np.meshgrid(np.arange(-maxlam,maxlam,maxlam/1000),np.arange(0,2*np,pi,np.pi/1000)) # also works
 r = np.array([
 	(rm*np.cos(spacelam)**2)*np.cos(spacelam)*np.cos(spacephi),
 	(rm*np.cos(spacelam)**2)*np.cos(spacelam)*np.sin(spacephi),
 	(rm*np.cos(spacelam)**2)*np.sin(spacelam)
-	]) # mathematics convention (latitude)
-# centrifugal acceleration = -n dot (w*(w*r))*n
-# gravitational acceleration = - n dot G*M*r/(np.linalg.norm(pos)**3)
-# mathematics convention (latitude) vector along magnetic field lines for every point (lambda, phi)
+	])
 # n = [-2*sin(lambda),cos(lambda),0] in [r,lambda,phi] coordinates
-# n = [-2*sin(lambda),cos(lambda),0]*[r;theta;phi]
+# n = [-2*sin(lambda),cos(lambda),0]*[r;lambda;phi]
 # if you put in other coordinates for r, theta, and phi, they become a matrix
 # that yields the below equation for n in cartesian coordinates
 n =-np.array([np.sin(spacelam)*np.cos(spacelam)*np.cos(spacephi),
@@ -40,7 +47,6 @@ ag = -G*M*np.linalg.vecdot(r,n,axis=0)/np.linalg.norm(r,axis=0)**3
 #for i in range(r.shape[1]):
 #    for j in range(r.shape[2]):
 #        acen[:,i,j] = -n[:,i,j]@np.cross(w,np.cross(w,r[:,i,j]))
-#w*(w*r)
 acen = np.linalg.vecdot(-n,np.cross(w,np.cross(w,r,axis=0),axis=0),axis=0)
 a = ag + acen
 plt.contourf(spacelam,spacephi,np.sign(ag+acen)*np.log10(np.abs(ag+acen)))
@@ -48,24 +54,18 @@ plt.xlabel("lambda / rad")
 plt.ylabel("phi / rad")
 plt.title("Contour plot of sgn(a)log10(|a|) / cm s^-2")
 plt.colorbar()
-lam0 = np.pi/2-np.atan(1/(np.sin(phi)*np.tan(beta))) # no problem
-lam0[lam0>=np.pi/2] -= np.pi # form a continuous line
-plt.plot(lam0,phi)
+plt.plot(phi[0,:],lam0[0,:],)
+plt.plot(phi[24,:],lam0[24,:])
+plt.plot(phi[49,:],lam0[49,:])
+plt.plot(phi[74,:],lam0[74,:])
+plt.legend([r'\omega t=0',r'\omega t=pi/2',r'\omega t=pi',r'\omega t=3pi/2'])
 plt.show()
 particle_phi = 0 # rad
-phi_idx = np.abs(phi-particle_phi).argmin() # index 1
-lam_idx = np.abs(lam-lam0[phi_idx]).argmin() # index 0
 v0 = +10^5 # cm s^-1
 t = 0
 dt = 0
 v = v0 # positive acceleration is in direction of increasing lambda
-dlam = maxlam/1000
-ds = rm*np.cos(lam)*np.sqrt(1+3*np.sin(lam)**2)*dlam
-while lam_idx > 0 | lam_idx < 1999:
-    # phi_idx is constant (no azimuthal motion)
-    # use formula rm*cos(lambda)*sqrt(1+3sin^2(lambda))dlambda
-    if np.sign(v) > 0:
-    dt = (v - np.sqrt(v^2 - 2*a[lam_idx,phi_idx]*ds[lam_idx]))/(2*a[lam_idx,phi_idx])
-    t += dt
-    v += a[lam_idx,phi_idx]*dt
-    
+#lam0[phi,wt]
+dlam = 
+ds = rm*np.cos(spacelam)*np.sqrt(1+3*np.sin(spacelam)**2)*dlam
+dt = 

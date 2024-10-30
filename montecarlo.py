2b7d6c4f8f6b9ee900ab0ad833543f459f50fc0c
vedoge <81337539+vedoge@users.noreply.github.com>
2024-10-30 19:30:33 +0530

    Finish montecarlo.py; set up simulation for next portion

--- montecarlo.py
+++ montecarlo.py
@@ -3,7 +3,7 @@ pi = np.pi
 sin = np.sin
 cos = np.cos
 tan = np.tan
-asin = np.asin                                                                                                                                                       
+asin = np.asin
 acos = np.acos
 atan = np.atan
 dot = np.linalg.vecdot
@@ -15,24 +15,25 @@ G = 6.67e-8 # dyn cm^2 g^-2
 M = 2.784e33 # g
 rpuls = 1e6 # cm
 rm = 1e8 # cm
-alpha 0.3 # rad
+alpha = 0.3 # rad
 beta = 0.5 # rad
 wmag = 2*pi*1 # radHz
-w = array([wmag*sin(beta),0,wmag*cos(beta)]) # vector for crossing 
+w = np.array([wmag*sin(beta),0,wmag*cos(beta)]) # vector for crossing
+tmax = 5 # s
+dt = 1e-5 # s
+i = 0 # indicates which iteration of the ode solver we are on
+# now we have calculated a starting chart
+phi0,wt0 = np.meshgrid(np.arange(0,2*pi,2*pi/100), np.arange(0,tmax*wmag, wmag*dt))
+lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi0) - np.cos(phi0)*np.sin(alpha)*np.sin(wt0) + np.cos(beta)*np.sin(alpha)*np.cos(wt0)*np.sin(phi0))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt0))
+v0 = 1e7*np.sign(lam0)
+phi = np.full_like (phi0, np.nan)
+lam = np.full_like(lam0, np.nan)
+v = np.full_like(v0,np.nan)
+phi[i,:] = phi0[i,:]
+lam[i,:] = lam0[i,:]
+v[i,:] = v0[i,:]
 t = 0 # s
-dt = 1e-4 # s
-# one particle per phi
-phi = np.arange(0,2*pi,pi/1000)
-wt = wmag*t # ie 0
-lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
-# choose 100 phi-cells for speed
-# recalculate lam0 every dt - it's less memory intensive than precalculating
-v0 = 1e7 # cm s^-1
-v = np.sign(lam0)*v0
-lam = lam0
-stopped = np.full_like(phi, False) # keep a record of the particles that have stopped 
-# eventually we will end up with 10^8 particles in the simulation (more than enough)
-while t <= 5:
+while t <= tmax:
 	r = np.array([
 	(rm*cos(lam)**2)*cos(lam)*cos(phi),
 	(rm*cos(lam)**2)*cos(lam)*sin(phi),
@@ -49,15 +50,10 @@ while t <= 5:
 	ds = v*dt + a*(dt**2)*0.5
 	v += a*dt
 	t += dt
+	i += 1
 	lam += ds/(rm*cos(lam)*sqrt(1 + 3*(sin(lam)**2)))
-	# add some n number of particles
-	wt = wmag*dt
-	lam = np.append(lam,np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt)))
-	# start the particles
-	phi = np.append(phi,np.arange(0,2*pi,np.pi/1000)
-# add 2e3 particles every 1e-4 s; 2e7 particles per second 
-
-
-
-
-
+	phi[i,:] = phi0[i,:]
+	lam[i,:] = lam0[i,:]
+	
+	v[i,:] = v0[i,:]
+	print(t)

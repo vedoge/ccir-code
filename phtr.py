3696140483fd9b58ed6eb2a5a2d33cb2f9569331
vedoge <81337539+vedoge@users.noreply.github.com>
2024-11-09 14:28:44 +0800

    Update phtr.py

--- phtr.py
+++ phtr.py
@@ -25,13 +25,16 @@ alpha, phi0 = np.meshgrid(np.arange(0,2*pi,pi/100),np.arange(0,2*pi,pi/100))
 phi = phi0
 uv = -u/tan(alpha-phi)
 u = np.full_like(phi,u)
+uf = np.zeros_Like(u)
+ufprime = np.zeros_like(uv)
+fphi = np.zeros_like(phi)
 ul = np.expand_dims(u.copy(),axis=2)
 stopped = np.full_like(phi,False,dtype=bool)
 while not np.all(stopped):
 	ua = 3*(u**2) - u
 	u += uv*dphi + 0.5*ua*(dphi**2)
 	uv += ua*dphi
-	phi += dphi
+	phi += dphi # check
 	ul = np.append(ul[:,:,:],u[:,:,np.newaxis],axis=2)
 	print(ul.shape[2],np.sum(stopped))
 	# correct fate: 
@@ -42,11 +45,19 @@ while not np.all(stopped):
 	# process the impact angle
 	stopped[idx] = True
 	# record du/dphi, record u, record phi for the impact
+	uf[idx] = u[idx]
+	ufprime[idx] = uv[idx]
+	fphi[idx] = phi[idx]
 	u[idx] = 0 # photon banished to infinity
 	uv[idx] = 0
-	# record photon impact
 	idx =  (u <= 0.5*rs/rm) | (u >= 0.5*rs/rpuls)
 	stopped[idx] = True
 	u[idx] = 0
 	uv[idx] = 0
-	
+# use the formula in the paper to calculate dy/dx for each of the curves
+rf = 0.5*rs/uf
+rprime = -0.5*rs/(ufprime**2)
+dydx_l = (rprime*cos(phi)-rf*sin(phi))/(rprime*sin(phi)+rf*cos(phi))
+rprime_b = rm*sin(2*phi)
+dydx_b = (rprime_b*cos(phi)-rf*sin(phi))/(rprime_b*sin(phi)+rf*cos(phi))
+dot = cos(atan(dydx_l) + atan(dydx_b)

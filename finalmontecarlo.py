8733c8a165b15525c2b84a48a926233cf63aeef6
Vedant Ganesh <>
2024-11-15 00:00:01 +0800

    autocommit

--- finalmontecarlo.py
+++ finalmontecarlo.py
@@ -17,9 +17,11 @@ dot = np.linalg.vecdot
 cross = np.cross
 norm = np.linalg.norm
 sqrt = np.sqrt
-import matplotlib.pyplot as plt
+# for multiprocessing
+import concurrent.futures
 # set up a stream of random numbers
 rng = np.random.Generator(np.random.SFC64())
+rand = rng.random
 G = 6.67e-8 # dyn cm^2 g^-2
 M = 2.784e33 # g
 rpuls = 1e6 # cm
@@ -32,37 +34,40 @@ w = np.array([wmag*sin(alpha),0,wmag*cos(alpha)])
 # define the array
 def s(lam):
 	return (rm/6)*(sqrt(3)*asinh(sqrt(3)*sin(lam)) + 3*sqrt(3)*sin(lam)*sqrt((sin(lam))**2 + 1/3))
-stot = s(maxlam)-s(0)
-print(stot)
-# s = np.linspace(0,stot,1e6) #~100 cm per cell 
-lam = np.zeros(int(1e6))
-for i in range(int(1e6)-1):
+def lat0(phi,wt):
+	return np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
+stot = s(maxlam)-s(-maxlam)
+def acc(lam,phi,arr):
+	r = np.array([
+		(rm*cos(lam)**2)*cos(lam)*cos(phi),
+		(rm*cos(lam)**2)*cos(lam)*sin(phi),
+		(rm*cos(lam)**2)*sin(lam)
+	]) # mathematics convention (latitude)
+	n = - np.array([
+			sin(lam)*cos(lam)*cos(phi),
+			sin(lam)*cos(lam)*sin(phi),
+			2*sin(lam)**2-cos(lam)**2
+	])
+	n = n/norm(n,axis=0) # normalise
+	# ag = -G*M*dot(r,n,axis=0)/norm(r,axis=0)**3
+	# acen = dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
+	return -G*M*dot(r,n,axis=0)/norm(r,axis=0)**3 + dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
+lam = np.zeros(int(1e5))
+lam[0] = -maxlam
+for i in range(int(1e5)-1):
 	l = lam[i]
 	lamprime = rm*cos(l)*sqrt(1+3*(sin(l)**2))
-	lam[i+1] = l + stot/(1e6*lamprime) # approx. 10^2 cm / cell; very reasonable computationally
-#plt.plot(s(lam))
-#plt.show()
+	lam[i+1] = l + stot/(1e5*lamprime) # approx. 2*10^3 cm / cell; very reasonable computationally
+
 # generate lambdas
-phi, lam = np.meshgrid(np.arange(0,2*pi,pi/500),lam)
+phi, lam = np.meshgrid(np.arange(0,2*pi,pi/50),lam)
 # now make things
-r = np.array([
-	(rm*np.cos(lam)**2)*np.cos(lam)*np.cos(phi),
-	(rm*np.cos(lam)**2)*np.cos(lam)*np.sin(phi),
-	(rm*np.cos(lam)**2)*np.sin(lam)
-	]) # mathematics convention (latitude)
-# centrifugal acceleration = -n dot (w*(w*r))*n
-# gravitational acceleration = - n dot G*M*r/(np.linalg.norm(pos)**3)
-# mathematics convention (latitude) vector along magnetic field lines for every point (lambda, phi)
-# n = [-2*sin(lambda),cos(lambda),0] in [r,lambda,phi] coordinates
-# n = [-2*sin(lambda),cos(lambda),0]*[r;theta;phi]
-# if you put in other coordinates for r, theta, and phi, they become a matrix
-# that yields the below equation for n in cartesian coordinates
-n = - np.array([np.sin(lam)*np.cos(lam)*np.cos(phi),
-				np.sin(lam)*np.cos(lam)*np.sin(phi),
-				2*np.sin(lam)**2-np.cos(lam)**2
-				])
-n = n/np.linalg.norm(n,axis=0) # normalise
-
-
-
-
+a = np.empty_like(phi)
+ac = np.empty.like(phi)
+with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+	a = 
+# now, we have acceleration for each lambda and phi
+t = 0
+lam0 = lat0(phi[0,:],wmag*t)
+# add N particles to each cell (add NM particles)
+# M = 100, N = 2

389bf89fbb814ba28a38dff361ee66cd3f96faf6
Vedant Ganesh <vedantg@beastiehmm.lan>
2024-11-17 22:32:22 +0800

    Synchronization commit

--- finalmontecarlo.py
+++ finalmontecarlo.py
@@ -1,3 +1,7 @@
+# finalmontecarlo.py
+# Main Monte Carlo Simulation code
+# full calculation 
+# use with montecarloroutines.py
 import numpy as np
 import matplotlib.pyplot as plt
 pi = np.pi
@@ -34,41 +38,55 @@ w = np.array([wmag*sin(alpha),0,wmag*cos(alpha)])
 # define the array
 def s(lam):
 	return (rm/6)*(sqrt(3)*asinh(sqrt(3)*sin(lam)) + 3*sqrt(3)*sin(lam)*sqrt((sin(lam))**2 + 1/3))
-def lat0(phi,wt):
+def lat0(phi,wt): # FIXME
 	return np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
 stot = s(maxlam)-s(-maxlam)
-def acc(lam,phi,arr):
+def update_region(region, dt):
+	global lam, phi
+	global a,plam,pphi,pv
+	lam_parts = plam.reshape(1,1,plam.size)
+	phi_parts = pphi.reshape(1,1,pphi.size)
+	# use the 2nd axis
+	# use an Event object to signal (releases GIL when thread is waiting)
+	# minimize each axis individually
+	print(phi.shape)
+	# correct the below with axis= args
+
+	lamcell = np.argmin(np.abs(lam_parts - lam[0,:,np.newaxis]))
+	phicell = np.argmin(np.abs(phi_parts - phi[:,0, np.newaxis]))
+	print(lamcell,phicell)
+def acc(lam,phi):
+	global a
 	r = np.array([
 		(rm*cos(lam)**2)*cos(lam)*cos(phi),
 		(rm*cos(lam)**2)*cos(lam)*sin(phi),
 		(rm*cos(lam)**2)*sin(lam)
 	]) # mathematics convention (latitude)
 	n = - np.array([
-			sin(lam)*cos(lam)*cos(phi),
-			sin(lam)*cos(lam)*sin(phi),
-			2*sin(lam)**2-cos(lam)**2
+		sin(lam)*cos(lam)*cos(phi),
+		sin(lam)*cos(lam)*sin(phi),
+		2*sin(lam)**2-cos(lam)**2
 	])
 	n = n/norm(n,axis=0) # normalise
 	# ag = -G*M*dot(r,n,axis=0)/norm(r,axis=0)**3
 	# acen = dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
-	arr = -G*M*dot(r,n,axis=0)/norm(r,axis=0)**3 + dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
+	a = -G*M*dot(r,n,axis=0)/norm(r,axis=0)**3 + dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
 lam = np.zeros(int(1e5))
 lam[0] = -maxlam
 for i in range(int(1e5)-1):
 	l = lam[i]
 	lamprime = rm*cos(l)*sqrt(1+3*(sin(l)**2))
 	lam[i+1] = l + stot/(1e5*lamprime) # approx. 2*10^3 cm / cell; very reasonable computationally
-
 # generate lambdas
 phi, lam = np.meshgrid(np.arange(0,2*pi,pi/50),lam)
+pphi = np.arange(0, 2*pi, pi/50)
+plam = np.zeros_like(pphi)
 # now make things
-global a;a = np.empty_like(phi)
-print(a)
-# ac = np.empty.like(phi)
-t1 = Thread(target=acc,args=(phi,lam,a))
-print(t1.run())
-print(a)
-# now, we have acceleration for each lambda and phi
+a = np.empty_like(phi)
+t1 = Thread(target=acc,args=(phi,lam))
+t1.run()
+t2 = Thread(target = update_region, args=(1.5,1e-5)) 
+t2.run()
 t = 0
 lam0 = lat0(phi[0,:],wmag*t)
 # add N particles to each cell (add NM particles)
--- montecarloroutines.py
+++ montecarloroutines.py
@@ -1,11 +1,8 @@
 # montecarloroutines.py
-# Monte Carlo Routines for montecarlo.py. 
+# Monte Carlo Routines for finalmontecarlo.py. 
 if __name__ == '__main__':
-    print("do not run this file directly")
-    raise EOFError
-# we need to record individual times for each thread
-# The mass accretion method will be done with a synchronize after each thread step
-# **kwargs dt passing?
+	print("do not run this file directly")
+#	raise EOFError
 # operations on python variables (not numpy variables) are serialized, so we can use this for coordination...? Utilize the GIL? 
 # We need a unified way of computing the mass accretion rate. 
 # Currently, my showerthoughts are: 
@@ -14,6 +11,16 @@ if __name__ == '__main__':
 # mass accretion rate
 # rad rate
 # use rad map to update acc map
-# profit
-def update_region(region, particle_lams, particle_phis):
+# region is a 2x2 array indexing 
 
+def update_region(region, dt):
+	global lam, phi
+	global a,plam,pphi,pv
+	lam_parts = plam.reshape(1,1,size(plam))
+	phi_parts = pphi.reshape(1,1,size(pphi))
+	# use the 2nd axis
+	# use an Event object to signal (releases GIL when thread is waiting)
+	# minimize each axis individually
+	lamcell = np.argmin(np.abs(lam_parts - lam[0,:,np.newaxis]))
+	phicell = np.argmin(np.abs(phi_parts - phi[:,0, np.newaxis]))
+	print(lamcell,phicell)

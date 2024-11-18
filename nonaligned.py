a60d9d3ddefaa50852bd6367e8cbc76b822b5945
vedoge <81337539+vedoge@users.noreply.github.com>
2024-11-18 08:43:13 +0800

    Add the namespace bindings to nonaligned.py

--- nonaligned.py
+++ nonaligned.py
@@ -1,5 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+pi = np.pi
+sin = np.sin
+cos = np.cos
+tan = np.tan
+asin = np.asin
+acos = np.acos
+atan = np.atan
+sinh = np.sinh
+cosh = np.cosh
+tanh = np.tanh
+asinh = np.asinh
+acosh = np.acosh
+atanh = np.atanh
+dot = np.linalg.vecdot
+cross = np.cross
+norm = np.linalg.norm
+sqrt = np.sqrt
 alpha = 0.3 #rad
 #10 KeV
 beta =  0.5 #rad
@@ -8,16 +25,16 @@ phi = np.arange(0,2*np.pi,np.pi/100)
 #phi = np.acos((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))/((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))**2 + (np.sin(chi)*(np.sin(alpha)*np.sin(beta) - np.cos(alpha)*np.cos(beta)*np.cos(wt)) - np.cos(beta)*np.cos(chi)*np.sin(wt))**2)**(1/2))
 #lam0 = np.acos(((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))**2 + (np.sin(chi)*(np.sin(alpha)*np.sin(beta) - np.cos(alpha)*np.cos(beta)*np.cos(wt)) - np.cos(beta)*np.cos(chi)*np.sin(wt))**2)**(1/2))
 wt = 0
-lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
+lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
 plt.plot(phi,lam0)
 wt = np.pi/2
-lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
+lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
 plt.plot(phi,lam0)
 wt = np.pi
-lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
+lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
 plt.plot(phi,lam0)
 wt = 3*np.pi/2
-lam0 = np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
+lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
 plt.plot(phi,lam0)
 plt.xlabel(r'$\varphi$ / rad')
 plt.ylabel(r'$\lambda_0$ / rad')

a811ffcc942056505797f746bbb6631ae4471717
vedoge <81337539+vedoge@users.noreply.github.com>
2024-11-18 14:12:15 +0800

    Fixed

--- nonaligned.py
+++ nonaligned.py
@@ -20,21 +20,18 @@ sqrt = np.sqrt
 alpha = 0.3 #rad
 #10 KeV
 beta =  0.5 #rad
-#chi,wt = np.meshgrid(np.arange(0,2*np.pi,np.pi/1000),np.arange(0,2*np.pi,np.pi/100))
 phi = np.arange(0,2*np.pi,np.pi/100)
-#phi = np.acos((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))/((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))**2 + (np.sin(chi)*(np.sin(alpha)*np.sin(beta) - np.cos(alpha)*np.cos(beta)*np.cos(wt)) - np.cos(beta)*np.cos(chi)*np.sin(wt))**2)**(1/2))
-#lam0 = np.acos(((np.cos(chi)*np.cos(wt) - np.cos(alpha)*np.sin(chi)*np.sin(wt))**2 + (np.sin(chi)*(np.sin(alpha)*np.sin(beta) - np.cos(alpha)*np.cos(beta)*np.cos(wt)) - np.cos(beta)*np.cos(chi)*np.sin(wt))**2)**(1/2))
 wt = 0
-lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
+lam0 = -asin(sin(phi)*(cos(alpha)*sin(beta) + cos(beta)*sin(alpha)*cos(wt)) - cos(phi)*sin(alpha)*sin(wt))
 plt.plot(phi,lam0)
 wt = np.pi/2
-lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
+lam0 = -asin(sin(phi)*(cos(alpha)*sin(beta) + cos(beta)*sin(alpha)*cos(wt)) - cos(phi)*sin(alpha)*sin(wt))
 plt.plot(phi,lam0)
 wt = np.pi
-lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
+lam0 = -asin(sin(phi)*(cos(alpha)*sin(beta) + cos(beta)*sin(alpha)*cos(wt)) - cos(phi)*sin(alpha)*sin(wt))
 plt.plot(phi,lam0)
 wt = 3*np.pi/2
-lam0 = asin((cos(alpha)*sin(beta)*sin(phi) - cos(phi)*sin(alpha)*sin(wt) + cos(beta)*sin(alpha)*cos(wt)*sin(phi))/(cos(alpha)*cos(beta) - sin(alpha)*sin(beta)*cos(wt)))
+lam0 = -asin(sin(phi)*(cos(alpha)*sin(beta) + cos(beta)*sin(alpha)*cos(wt)) - cos(phi)*sin(alpha)*sin(wt))
 plt.plot(phi,lam0)
 plt.xlabel(r'$\varphi$ / rad')
 plt.ylabel(r'$\lambda_0$ / rad')

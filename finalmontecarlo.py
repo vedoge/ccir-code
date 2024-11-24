55cd1cab6ee4bf76ac91e91c19aec43f68cbb03c
Vedant Ganesh <>
2024-11-25 00:00:01 +0800

    autocommit

--- finalmontecarlo.py
+++ finalmontecarlo.py
@@ -36,15 +36,14 @@ alpha = 0.3 # rad
 beta = 0.5 # rad
 wmag = 2*pi*1 # radHz
 # computing parameters
-nr_compute_threads = 4 # customize to your needs
+nr_compute_threads = 8 # customize to your needs
 w = np.array([wmag*sin(-beta),0,wmag*cos(-beta)])
 # define the array
 def s(lam):
 	return (rm/6)*(sqrt(3)*asinh(sqrt(3)*sin(lam)) + 3*sqrt(3)*sin(lam)*sqrt((sin(lam))**2 + 1/3))
 
 def lat0(phi,wt): # FIXME
-	return np.asin(np.cos(alpha)*np.sin(beta)*np.sin(phi) - np.cos(phi)*np.sin(alpha)*np.sin(wt) + np.cos(beta)*np.sin(alpha)*np.cos(wt)*np.sin(phi))/(np.cos(alpha)*np.cos(beta) - np.sin(alpha)*np.sin(beta)*np.cos(wt))
-
+	return -asin(sin(phi)*(cos(alpha)*sin(beta) + cos(beta)*sin(alpha)*cos(wt)) - cos(phi)*sin(alpha)*sin(wt))
 stot = s(maxlam)-s(-maxlam)
 
 def update_region(plam,pphi,pv):
@@ -78,21 +77,16 @@ def acc(lam,phi):
 	# acen = dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
 	return -G*M*dot(r,n,axis=0)/(norm(r,axis=0)**3) + dot(-n,cross(w,cross(w,r,axis=0),axis=0),axis=0)
 
-'''
-running_acc_threads_mutex.acquire()
-running_acc_threads -= 1
-running_acc_threads.release()
-'''
-
-lam = np.zeros(int(1e5))
+lam = np.zeros(int(5e5))
 lam[0] = -maxlam
-for i in range(int(1e5)-1):
+for i in range(int(5e5)-1):
 	l = lam[i]
 	lamprime = rm*cos(l)*sqrt(1+3*(sin(l)**2))
-	lam[i+1] = l + stot/(1e5*lamprime) # approx. 2*10^3 cm / cell; very reasonable computationally
+	lam[i+1] = l + stot/(5e5*lamprime) # approx. 4*10^2 cm / cell; very reasonable computationally
 
 # generate lambdas
-lam,phi = np.meshgrid(lam,np.arange(0,2*pi,pi/50)) # granularity of phi reasonable
+phi_width = pi/250
+lam,phi = np.meshgrid(lam,np.arange(0,2*pi,phi_width)) # granularity of phi reasonable
 # lam[0,:] is increasing lambda
 # phi[:,0] is increasing phi
 a = np.empty_like(phi)
@@ -103,31 +97,32 @@ with ThreadPoolExecutor(max_workers = nr_compute_threads) as executor:
 	t = 0 # s
 	dt = 1e-4 # s (initial)
 	j = 0
-	tfin = 2*dt # s
+	tfin = 2 # s
 	dMdt = np.zeros(int(tfin / dt)+1)
-	partspercell = 10 # 500 particles per 10^-5 s (5e7 particles per second, or in other words, 2e10 g per particle) 
-	Mpart = 1e16/(phi.shape[0]*partspercell) # mass of individual particle
-	pphi = np.arange(0,2*pi,pi/50)
+	partspercell = 10
+	cells = phi[:,0].size
+	dMdt_disk = 1e15 # g s^-1
+	Mpart = dMdt*dt/(phi.shape[0]*partspercell) # mass of individual particle
+	pphi = rand(size=(cells,partspercell))*phi_width + phi[:,0:(partspercell)]
 	plam = lat0(pphi,0)
 	pv = np.sign(plam)*1e7
 	while t <= tfin:
-		plam = np.array([_ for _ in executor.map(update_region,plam,pphi,pv)])
-		lost = np.where(np.abs(plam)>=maxlam,True,False) # doesn't change size...? 
-		print(t,lost.shape)
+		plam = np.array([i for i in executor.map(update_region,plam,pphi,pv)])
+		print(np.sum(np.where((plam>=maxlam) + (plam <= -maxlam))))
+		dMdt[j] = np.sum(np.where((plam >= maxlam) | (plam <= -maxlam)))*Mpart
+		if dMdt[j] != dMdt[j-1]:
+			print(dMdt[j])
+		pphi[np.where((plam >= maxlam) | (plam <= -maxlam))] = np.nan
+		plam[np.where(pphi == np.nan)] = np.nan
 		#print(np.max(np.abs(plam)))
-		dMdt[j] = np.sum(lost)*Mpart
-		#if dMdt[j]:
-			#print(t,dMdt[j])
 		# add new particles 
-		pphi = pphi[lost]
-		plam = plam[lost]
 		t += dt
 		j += 1
-		pphi = np.append(pphi, phi[:,0].copy())
-		plam = np.append(plam, lat0(phi[:,0], wmag*t))
-		# add N particles to each cell (add NM particles)
-		# M = 100, N = 2
-		# you need to multithread the update function in the same way, and just finish the rest of the simulation
-		# you're very close.
+		pphi = np.extract(pphi,np.where(np.isfinite(plam)))
+		plam = np.extract(plam,np.where(np.isfinite(plam)))
+		pphi_delta =  rand(size=partspercell*cells)*phi_width
+		pphi_delta += np.repeat(phi[:,0],partspercell).flatten()
+		plam = np.append(plam, lat0(pphi_delta, wmag*t).copy())
 	plt.plot(np.arange(0,tfin+dt,dt),dMdt)
+	plt.savefig('fig.eps')
 	plt.show()
